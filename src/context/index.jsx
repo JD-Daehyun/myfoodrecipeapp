138f0f952cf7568db9a68998e89a31459e530088
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-11 13:40:03 -0700

    Add API to context index.jsx

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -4,5 +4,16 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
     const [searchParam, setSearchParam] = useState('');
-  return <GlobalContext.Provider value={{searchParam, setSearchParam}}>{children}</GlobalContext.Provider>;
+    async function handleSubmit(event){
+        event.preventDefault();
+        try{
+            const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
+            const data = await response.json();
+            console.log(data)
+
+        }catch(e){
+            console.log(e);
+        }
+    }
+  return <GlobalContext.Provider value={{searchParam, setSearchParam, handleSubmit}}>{children}</GlobalContext.Provider>;
 }

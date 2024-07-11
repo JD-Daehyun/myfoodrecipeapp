e74db1370fe48a97ea1ba7e4d7b7b2301c56af63
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-11 14:12:10 -0700

    Feature Recipe Item after search

---
+++ src/components/recipe-item/index.jsx
@@ -0,0 +1,8 @@
+
+
+
+
+export default function RecipeItem({item}){
+
+    return <div className="flex flex-col w-80 overflow-hidden p-5 bg-white/75 shadow-xl gap-5 border-2 rounded-2xl border-white">RecipeITem</div>
+}
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,22 @@
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
+export default function Home() {
+  const { loading, recipeList } = useContext(GlobalContext);
 
-
-
-export default function Home(){
-
-    return <div>
-        Home
+  if (loading) {
+    return <h1>Loading!</h1>;
+  }
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">No Recipe Found</p>
+        </div>
+      )}
     </div>
+  );
 }

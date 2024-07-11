bec66de271033454fa4056f23be0935e6a23c2b7
Daehyun Jung <daehyunj1997@gmail.com>
2024-07-11 12:48:44 -0700

    Add: Feature NavBar with Tailwind Css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/home/index.jsx";
+import Favorites from "./pages/favorites";
+import Details from "./pages/details";
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <div className="min-h-screen p-6 bg-white text-gray-600 text-lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          {/* dynamic routing for detail page since each item has its own page */}
+          <Route path="/recipe-item/:id" element={<Details />} />
+        </Routes>
+      </div>
     </div>
   );
 }
